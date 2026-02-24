ff17c32ea195d41b18d3194063a3717bd15b9f13
bariseser <bariseser@gmail.com>
2026-02-24 13:23:19 +0300

    feat: introduce another critical error (nil map assignment) in healthHandler

--- main.go
+++ main.go
@@ -32,6 +32,10 @@ func pingHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func healthHandler(w http.ResponseWriter, r *http.Request) {
+	var meta map[string]string
+	// Critical Error: Assignment to entry in nil map will cause a panic
+	meta["version"] = "v1"
+
 	response := HealthResponse{Success: true, Timestamp: time.Now().Unix()}
 
 	w.Header().Set("Content-Type", "application/json")
