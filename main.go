ff091bb1daf450124152a4078d56381df723035c
bariseser <bariseser@gmail.com>
2026-02-24 12:24:57 +0300

    feat: add health endpoint

--- main.go
+++ main.go
@@ -4,10 +4,16 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"time"
 )
 
 type PingResponse struct {
 	Message string `json:"message"`
+}
+
+type HealthResponse struct {
+	Success   bool  `json:"success"`
+	Timestamp int64 `json:"timestamp"`
 }
 
 func pingHandler(w http.ResponseWriter, r *http.Request) {
@@ -21,8 +27,20 @@ func pingHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func healthHandler(w http.ResponseWriter, r *http.Request) {
+	response := HealthResponse{Success: true, Timestamp: time.Now().Unix()}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+
+	if err := json.NewEncoder(w).Encode(response); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
 func main() {
 	http.HandleFunc("/ping", pingHandler)
+	http.HandleFunc("/health", healthHandler)
 
 	log.Println("Server is starting on port 3000...")
 	if err := http.ListenAndServe(":3000", nil); err != nil {
